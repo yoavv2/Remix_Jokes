f1e139ebbe62ac8e1cbc1a9b0c1711c6e13ca772
yoav <23hutc8@gmail.com>
2022-04-02 19:54:00 +0300

    [Authentication, Unexpected Errors] Done

---
+++ app/routes/index.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'remix';
+import type { LinksFunction } from 'remix';
+
+import stylesUrl from '~/styles/index.css';
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: stylesUrl }];
+};
+export default function IndexRoute() {
+  return (
+    <div className='container'>
+      <div className='content'>
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to='jokes'>Read Jokes</Link>
+            </li>
+            <li>
+              <Link reloadDocument to='/jokes.rss'>
+                RSS
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
